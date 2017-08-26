3776123d5ce235c9e2f666bd6dbb198de5a4af8e
Niclas Kempe <niclas.kempe@gmail.com>
2017-08-27 01:08:14 +0200

    Removed some comments and adjusted indentation

--- Area.cpp
+++ Area.cpp
@@ -16,40 +16,37 @@ using namespace std;
 
 namespace AdventureWorld
 {
-	
+
 	Area::Area() : north(0), east(0), west(0), south(0)
 	{
 		random_name();
 		character_vec = new vector<Character*>();
-                item_vec = new vector<Item*>();
-                cout << "Area created " << endl;
+        item_vec = new vector<Item*>();
+        cout << "Area created " << endl;
 	}
-	
+
 	Area::~Area()
 	{
-        
-        }
+
+    }
+
 	string Area::get_name() const
 	{
 		return this->name;
 	}
-	
+
 	void Area::spawn_character(Character * c)
 	{
-                //character_vec = new vector<Character*>();
-                
 		character_vec->push_back(c);
-                this->num_of_chars++;
+        this->num_of_chars++;
 	}
-        
-        void Area::spawn_item(Item * i) //TODO: Add similar functionality as for characters, templates?
+
+    void Area::spawn_item(Item * i) //TODO: Add similar functionality as for characters, templates?
 	{
-                //character_vec = new vector<Character*>();
 		item_vec->push_back(i);
-                //this->num_of_chars++;
 	}
-        
-	
+
+
 	void Area::remove_character(Character * c)
 	{
             vector<Character*>::iterator it;
@@ -63,28 +60,28 @@ namespace AdventureWorld
                     break;
                 }
             }
-            
+
 	}
-        
-        void Area::print_neighbours() const
-        {
-             if (this->north != 0)
-                        cout << "north ->1 " << (this->north)->get_name() << endl; 
-                if (this->east != 0)
-                        cout << "east -> " << (this->east)->get_name() << endl;
-                if (this->west != 0)
-                        cout << "west -> " << (this->west)->get_name() << endl; 
-                if (this->south != 0)
-                        cout << "south -> " << (this->south)->get_name() << endl;
-        }
-        
-        bool Area::clear_corpses()
+
+    void Area::print_neighbours() const
+    {
+            if (this->north != 0)
+                    cout << "north ->1 " << (this->north)->get_name() << endl;
+            if (this->east != 0)
+                    cout << "east -> " << (this->east)->get_name() << endl;
+            if (this->west != 0)
+                    cout << "west -> " << (this->west)->get_name() << endl;
+            if (this->south != 0)
+                    cout << "south -> " << (this->south)->get_name() << endl;
+    }
+
+    bool Area::clear_corpses()
 	{
             vector<Character*>::iterator it;
-          
+
             for(it = character_vec->begin(); it != character_vec->end(); ++it)
             {
-             
+
                 if ((*it)->life() == 0)
                 {
                     it = character_vec->erase(it);
@@ -94,8 +91,8 @@ namespace AdventureWorld
             }
             return false;
 	}
-        
-        bool Area::is_character_present(Character * c) const
+
+    bool Area::is_character_present(Character * c) const
 	{
             vector<Character*>::iterator it;
 
@@ -108,7 +105,7 @@ namespace AdventureWorld
             }
             return false;
 	}
-	
+
 	void Area::print_characters() const
 	{
             vector<Character*>::iterator it;
@@ -118,58 +115,57 @@ namespace AdventureWorld
                     cout << (*it)->name() << "(" << (*it)->type() << ")" << endl;
             }
 	}
-        
-        int Area::num_of_characters() const
+
+    int Area::num_of_characters() const
 	{
             return num_of_chars;
 	}
-        
-        
-	
+
+
+
 	string Area::description() const
 	{
-		string desc = "this is an area";
-		return desc;
+    		string desc = "this is an area";
+    		return desc;
 	}
-	
+
 	void Area::random_name()
 	{
 		int num = rand() % 4 + 1;
 		 switch ( num ) {
-		  case 1:            // Note the colon, not a semicolon
+		  case 1:
 			this->name = "The Misty Mountains  ";
 			break;
-		  case 2:            // Note the colon, not a semicolon
+		  case 2:
 			this->name = "The Dark Swamps      ";
 			break;
-		  case 3:            // Note the colon, not a semicolon
+		  case 3:
 			this->name = "The Barren Wastelands";
 			break;
-		  case 4:            // Note the colon, not a semicolon
+		  case 4:
 			this->name = "The Black Forest ";
 			break;
-		  default:            // Note the colon, not a semicolon
+		  default:
 			cout << "Error! Wrong in randomization, number was " << num << endl;
 			break;
 		  }
-		
-		
+
+
 	}
-        
+
         bool Area::operator==(const Area &other) const {
             return (*this == other);
         }
-        
-       
-	
+
+
+
 
     ostream& operator<< (ostream &out, Area &a)
     {
         // Since operator<< is a friend of the Point class, we can access
         // Point's members directly.
         out << a.get_name() << "(" <<
-            a.num_of_characters() << ")" << endl;
+        a.num_of_characters() << ")" << endl;
         return out;
     }
-}	
-	
+}
